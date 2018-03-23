b15e74a78d5c2816bcfc9047aa5cdbcd6bbf1d41
donrestarone <shashikejayatunge@gmail.com>
2018-03-23 18:19:05 -0400

    complete cats exercise

---
+++ cats/cat.rb
@@ -0,0 +1,47 @@
+class Cat
+	def initialize(name, preferred_food, meal_time)
+		@name = name
+		@preferred_food = preferred_food
+		@meal_time = meal_time
+	end
+
+	def eats_at
+		if @meal_time < 12 
+			puts "#{@meal_time} AM"
+		else 
+
+			puts "#{@meal_time - 12} PM "
+			
+		end 
+	end 
+	def meow
+		if @meal_time < 12
+			return "my name is #{@name} and i like eating #{@preferred_food} at #{eats_at} AM"
+		elsif @meal_time > 12 
+			return "My name is #{@name} and I eat #{@preferred_food} at #{ (@meal_time - 12)} PM"
+		else 
+			return "this is broken"
+		end 
+
+	end
+	
+
+
+end 	
+
+tiberius = Cat.new('tiberius', 'kibbles', 19)
+
+tom = Cat.new('tom', 'fish', 19)
+
+puts "cat #{tiberius.inspect}"
+
+
+
+
+puts "cat #{tom.inspect}"
+
+
+
+puts tiberius.meow
+puts tom.meow
+
