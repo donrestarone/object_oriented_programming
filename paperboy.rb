6a8f9f053e5e34324c3244b1c8a11cbb1b6ab5bd
donrestarone <shashikejayatunge@gmail.com>
2018-03-23 19:50:08 -0400

    this is all im going to get done on a friday night

--- paperboy.rb
+++ paperboy.rb
@@ -6,12 +6,30 @@ class Paperboy
 	end 
 
 	def quota
+		minimum =  (@experience / 2) + 50
+		return minimum
 
+	end 
 
+	def deliver(start_address, end_address)
+	delivaries = (start_address - end_address)
+	@earnings = delivaries * 0.25
+		if delivaries > minimum
+			@earnings = (delivaries - minimum) * 50 
+			return 
 
+		end
+
+	end
+
+	def report 
 	end 
 end 
 
-bob = Paperboy.new('bob', 'intermediate', 50)
+bob = Paperboy.new('bob', 150, 50)
 
 puts bob.inspect
+
+puts bob.quota
+
+bob.deliver(50, 150)
