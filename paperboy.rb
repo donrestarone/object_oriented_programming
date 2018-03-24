d2bdf7a6c6e36917a22b10c27b22f98f033b55e8
donrestarone <shashikejayatunge@gmail.com>
2018-03-24 12:23:54 -0400

    half way through paperboy exercise

--- paperboy.rb
+++ paperboy.rb
@@ -1,35 +1,67 @@
 class Paperboy 
 	def initialize(name, experience, earnings)
 		@name = name 
-		@experience = experience
+		@experience = experience 
 		@earnings = earnings 
+
+		@minimum = 50
+		@number_of_del = 0
 	end 
 
 	def quota
-		minimum =  (@experience / 2) + 50
-		return minimum
-
+		# @experience += 1 
+		@minimum = (@experience / 2) + 50  
+		
+		return @minimum
 	end 
 
 	def deliver(start_address, end_address)
-	delivaries = (start_address - end_address)
-	@earnings = delivaries * 0.25
-		if delivaries > minimum
-			@earnings = (delivaries - minimum) * 50 
-			return 
+		@experience += 1 + (start_address - end_address).abs #+1 to count the first house
+		@number_of_del = 1+ (start_address - end_address).abs #+1 to count the first house
+		@earnings += @number_of_del * 0.25
+			if @number_of_del > @minimum
+				@earnings += (number_of_del - @minimum) * 0.50 
+				return @earnings
 
-		end
 
-	end
+
+			end
+	end 
 
 	def report 
+		p "my name is #{@name} and I made #{@number_of_del} delivaries and earned $#{@earnings} "
 	end 
 end 
 
-bob = Paperboy.new('bob', 150, 50)
+
+#testing 
+#methodology. 
+#variable name bob. starting arguments ('bob', 150, 0)
+#make 10 delivaries 
+#check quota twice 
+
+
+
+bob = Paperboy.new('bob', 50, 0)
 
 puts bob.inspect
 
-puts bob.quota
+bob.deliver(1, 10)
+ bob.quota
 
-bob.deliver(50, 150)
+
+bob.deliver(1, 10)
+puts bob.inspect
+ 
+ bob.quota
+
+bob.deliver(1, 10)
+puts bob.inspect
+
+ 
+ bob.quota
+
+bob.deliver(1, 10)
+puts bob.inspect
+
+puts bob.report
