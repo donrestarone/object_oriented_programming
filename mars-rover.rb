b1a15d986e087f1163862f1652c1171e92255610
donrestarone <shashikejayatunge@gmail.com>
2018-03-25 13:04:53 -0400

    finish rover assignment, need to get grid working

--- mars-rover.rb
+++ mars-rover.rb
@@ -18,4 +18,79 @@ end
 
 puts rover_grid
 
+class Marsrover
+  def initialize(identifier, x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+    @identifier = identifier
+  end 
+
+  def read_instructions
+      if @direction == "l"
+        p "the rover is moving left"
+      elsif @direction == "r"
+        p "the rover is moving right"
+
+      elsif @direction == "m"
+        p "rover is moving forward"
+        
+      else 
+        p "the rover is not moving in that direction"
+
+      end 
+  end 
+
+  def give_instructions(turning_direction)
+      if turning_direction == "l"
+        @direction = turning_direction
+      elsif turning_direction == "r"
+        @direction = turning_direction
+      else 
+        p "error"
+        
+      end
+  end
+
+
+  def moverover(x_dir, y_dir)
+    @x += x_dir
+    @y += y_dir
+
+  end 
+
+  def roverstatus 
+    status_direction = ""
+    if @direction == "l"
+      status_direction = "left"
+    elsif 
+      @direction == "r"
+      status_direction = "right"
+    end
+    puts "#{@identifier} is stationed at x#{@x}, y#{@y} and is facing #{status_direction}"
+
+  end
+
+end
+
+rover1 = Marsrover.new("rover one", 2, 2, "l")
+puts "report 1"
+puts rover1.roverstatus
+puts ""
+
+rover1.give_instructions('r')
+puts "report 2"
+puts rover1.roverstatus
+puts ""
+
+rover1.moverover(2, 2)
+puts "report 2"
+puts rover1.roverstatus
+puts ""
+
+
+
+
+
+
  
